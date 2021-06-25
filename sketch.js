30b7302a3e9fc7d9920c50ac32f50ca08139fe4e
ilikechese <joshua.yeo69@gmail.com>
2021-06-25 10:22:40 +0800

    added text

--- sketch.js
+++ sketch.js
@@ -37,7 +37,8 @@ function homescreen1(){
         background(255,255,255)
         fill(215, 215, 215)
         rect(22,24,183,35)
-        let s = 'Food For All';
+        textSize(16)
+        let s = 'FoodForAll';
         fill(0,0,0)
         text(s, 30, 40, 70, 80); // Text wraps within text box
 
@@ -62,17 +63,26 @@ function homescreen1(){
       function loginbutton(){
         fill(249, 247, 230)
         rect(230,24,57,35)
-        let a = 'ABOUT US';
+        textSize(15)
+        let a = 'login';
         fill(0,0,0)
-        text(a, 30, 80, 70, 80);
+        text(a, 245, 35, 70, 150)
       }
+
       function aboutus(){
         fill(255, 249, 238)
         rect(22,87,141,152)
+        textSize(13)
+        let b = 'we are trying to help people eat and get fat, mainly for poor people'
+        fill(0,0,0)
+        text(b, 25, 90 ,70, 150)
       }
       function why(){
         fill(248, 244, 231)
         rect(176,260,111,137)
+        let c = 'we want everyone to have food'
+        fill(0,0,0)
+        text(c, 180, 265 ,70, 150)
       }
       function base(){
         fill(196, 196, 196)
@@ -81,10 +91,17 @@ function homescreen1(){
       function order(){
         fill(250,250,250)
         rect(29,427,119,15)
+        let d = 'food form'
+        fill(0,0,0)
+        text(d,32, 430, 70, 150)
       }
       function terms(){
         fill(250,250,250)
         rect(163,427,119,15)
+        textSize(7)
+        let e = 'tems and conditions'
+        fill(0,0,0)
+        text(e,180, 433, 70, 150)
       }
 
 
